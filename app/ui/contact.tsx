d678e38b850da3be3280aef916f3dc9519a899a0
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-01-26 22:24:23 -0300

    add contact links

--- app/ui/contact.tsx
+++ app/ui/contact.tsx
@@ -2,13 +2,38 @@ export default function Contact() {
   return (
     <div className="flex flex-col py-5 px-10 bg-[#FFEFD6]" id="contact-me">
       <h1 className="text-3xl mb-5">Contact-me</h1>
-      <p>Whatsapp: 54 9 9612-0527</p>
-      <p>E-Mail: jorgem.eberhardc@gmail.com</p>
+      <p>
+        Whatsapp:{" "}
+        <a className="hover:text-[#0E5E6F]" href="https://wa.me/5554996120527">
+          (54) 9 9612-0527
+        </a>
+      </p>
+      <p>
+        E-Mail:{" "}
+        <a
+          className="hover:text-[#0E5E6F]"
+          href="mailto:jorgem.eberhardc@gmail.com"
+        >
+          jorgem.eberhardc@gmail.com
+        </a>
+      </p>
       <p>
         Linkedin:
-        <a href="https://www.linkedin.com/in/jorge-eberhard/">
+        <a
+          className="hover:text-[#0E5E6F]"
+          href="https://www.linkedin.com/in/jorge-eberhard/"
+        >
           {" "}
-          Jorge Eberhard
+          Jorge Miguel Eberhard da Conceição
+        </a>
+      </p>
+      <p>
+        Github:{" "}
+        <a
+          className="hover:text-[#0E5E6F]"
+          href="https://github.com/jorgeeberhard"
+        >
+          jorgeeberhard
         </a>
       </p>
     </div>
