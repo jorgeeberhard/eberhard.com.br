ff5c5902af92462f1dc9ba2d3b4893ecf5a1c568
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-02-02 13:49:35 -0300

    style: change contact style

--- app/ui/contact.tsx
+++ app/ui/contact.tsx
@@ -1,4 +1,6 @@
 import { useTranslations } from "next-intl";
+import { FaLinkedin, FaWhatsapp, FaGithub } from "react-icons/fa";
+import { IoMailOutline } from "react-icons/io5";
 
 export default function Contact() {
   const translations = useTranslations("contact");
@@ -6,40 +8,39 @@ export default function Contact() {
   return (
     <div className="flex flex-col py-5 px-10 bg-[#FFEFD6]" id="contact-me">
       <h1 className="text-3xl mb-5">{translations("title")}</h1>
-      <p>
-        Whatsapp:{" "}
+      <div>
         <a className="hover:text-[#0E5E6F]" href="https://wa.me/5554996120527">
+          <FaWhatsapp className="inline mr-2" />
           (54) 9 9612-0527
         </a>
-      </p>
-      <p>
-        E-Mail:{" "}
+      </div>
+      <div>
         <a
           className="hover:text-[#0E5E6F]"
           href="mailto:jorgem.eberhardc@gmail.com"
         >
+          <IoMailOutline className="inline mr-2" />
           jorgem.eberhardc@gmail.com
         </a>
-      </p>
-      <p>
-        Linkedin:
+      </div>
+      <div>
         <a
           className="hover:text-[#0E5E6F]"
           href="https://www.linkedin.com/in/jorge-eberhard/"
         >
-          {" "}
+          <FaLinkedin className="inline mr-2" />
           Jorge Miguel Eberhard da Conceição
         </a>
-      </p>
-      <p>
-        Github:{" "}
+      </div>
+      <div>
         <a
           className="hover:text-[#0E5E6F]"
           href="https://github.com/jorgeeberhard"
         >
+          <FaGithub className="inline mr-2" />
           jorgeeberhard
         </a>
-      </p>
+      </div>
     </div>
   );
 }
