8521f02dcb6f1ad426cc418abbf0cbbfa0bc161d
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-02-02 11:44:24 -0300

    style: change the `stack` style

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -14,4 +14,83 @@ export const repos = [
   "h2o-erlang",
 ];
 
+type Technology = {
+  name: string;
+  description: string;
+  link: string;
+};
+
+type Stack = {
+  name: string;
+  tech: Technology[];
+};
+
+export const stack: Stack[] = [
+  {
+    name: "Front-end",
+    tech: [
+      {
+        name: "react",
+        description: "React",
+        link: "https://react.dev",
+      },
+      {
+        name: "tailwindcss",
+        description: "TailwindCSS",
+        link: "https://tailwindcss.com/",
+      },
+      {
+        name: "typescript",
+        description: "TypeScript",
+        link: "https://www.typescriptlang.org/",
+      },
+    ],
+  },
+  {
+    name: "Back-end",
+    tech: [
+      {
+        name: "nodejs",
+        description: "Node JS",
+        link: "https://nodejs.org/",
+      },
+      {
+        name: "typescript",
+        description: "TypeScript",
+        link: "https://www.typescriptlang.org/",
+      },
+      {
+        name: "nextjs",
+        description: "NextJS",
+        link: "https://nextjs.org/",
+      },
+      {
+        name: "postgresql",
+        description: "PostgreSQL",
+        link: "https://www.postgresql.org/",
+      },
+    ],
+  },
+  {
+    name: "DevOps",
+    tech: [
+      {
+        name: "git",
+        description: "Git",
+        link: "https://git-scm.com/",
+      },
+      {
+        name: "github",
+        description: "Github Actions",
+        link: "https://github.com/features/actions",
+      },
+      {
+        name: "docker",
+        description: "Docker",
+        link: "https://www.docker.com/",
+      },
+    ],
+  },
+];
+
 export type Locales = "en" | "pt-BR";
--- app/ui/stack.tsx
+++ app/ui/stack.tsx
@@ -1,50 +1,34 @@
+import { stack } from "@/app/lib/definitions";
+import Link from "next/link";
 import Image from "next/image";
+
 export default function Stack() {
   return (
     <div className="flex flex-col py-5 px-10 bg-[#FFEFD6]">
       <h1 className="text-3xl">Stack</h1>
-      <div className="grid grid-cols-6 pt-2">
-        <Image
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"
-          width={50}
-          height={50}
-          alt="NextJS"
-        />
-
-        <Image
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
-          width={50}
-          height={50}
-          alt="TypeScript"
-        />
-
-        <Image
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
-          width={50}
-          height={50}
-          alt="Git"
-        />
-
-        <Image
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
-          width={50}
-          height={50}
-          alt="NodeJS"
-        />
-
-        <Image
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-          width={50}
-          height={50}
-          alt="TailwindCSS"
-        />
-
-        <Image
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
-          width={50}
-          height={50}
-          alt="Postgres"
-        />
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 sm:gap-2 justify-items-center content-center pt-2">
+        {stack.map((domain, index) => (
+          <div
+            className="w-full lg:w-2/3 px-3 py-2 bg-[#F2DEBA] shadow-md rounded-md"
+            key={index}
+          >
+            <h1 className="text-xl">{domain.name}</h1>
+            {domain.tech.map((tech, index) => (
+              <div key={index} className="flex items-center">
+                <Image
+                  key={index}
+                  src={`/icons/${tech.name}.svg`}
+                  height={48}
+                  width={48}
+                  alt={tech.name}
+                />
+                <Link className="ml-2" href={tech.link}>
+                  {tech.description}
+                </Link>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
